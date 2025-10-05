037055aa24df00ea8404a4e5296eeb3285d70bd2
dayat <van.daytch@gmail.com>
2025-10-05 19:25:37 +0700

    fixing typo port

--- backend/server/server-start.ts
+++ backend/server/server-start.ts
@@ -10,7 +10,7 @@ const serverStart = async () => {
 
   console.log("ENV", process.env.NODE_ENV);
 
-  const httpPort = process.env.HTTP_PORT || process.env.PORT || 7011;
+  const httpPort = process.env.HTTP_PORT || process.env.PORT || 7001;
   const httpsPort = process.env.HTTPS_PORT || 7001
 
   if (process.env.NODE_ENV === "production" && process.env.SSL === "true") {
